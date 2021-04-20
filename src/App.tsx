b6e92dbf05a73540402dca0f09671f86db519920
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-20 13:12:32 +0530

    Tailwind css added

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App flex">
+        <div className="Content h-full bg-black">
+          Scam
+        </div>
+        <div className="NavSidebar h-full">
+          Nav
+        </div>
+    </div>
+  );
+}
+
+export default App;
