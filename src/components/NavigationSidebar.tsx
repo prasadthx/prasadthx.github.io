466572abec8dea99882bdc8eddcdd1fe12e27477
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-21 19:41:09 +0530

    Sidebar navigation styles fixed

--- src/components/NavigationSidebar.tsx
+++ src/components/NavigationSidebar.tsx
@@ -1,23 +1,68 @@
+import { GoPrimitiveDot } from 'react-icons/go'
+import { AiFillHome, TiPointOfInterest, AiFillSafetyCertificate, AiOutlineFundProjectionScreen } from "react-icons/all";
+import {useState} from "react";
+import './NavigationSidebar.css'
 
+let classState;
 const href = [
-    '#home',
-    '#interests',
-    '#certificate',
-    '#projects'
+    [   '#home',
+        'Home',
+        <AiFillHome/>
+    ],
+    [
+        '#interests',
+        'Interests',
+        <TiPointOfInterest/>
+    ],
+    [
+        '#certificate',
+        'Certifications',
+        <AiFillSafetyCertificate/>
+    ],
+    [
+        '#projects',
+        'Projects',
+        <AiOutlineFundProjectionScreen/>
+    ]
 ]
 
 export const NavigationSidebar = (props:any) => {
+    const [hoverState, setHoverState] = useState(false);
+    let pageIndex = props.currentPage
+    let classState = hoverState ? 'visible' : 'invisible'
+    let dotInvisible = hoverState ? 'invisible' : 'visible'
     return (
-        <div className='w-full h-full'>
-            <li><a href="#home">{props.currentPage}</a></li>
-            <li><a href="#interests">Interests</a></li>
-            <li><a href="#certificate">Certifications</a></li>
-            <li><a href="#projects">Projects</a></li>
+        <div
+            className='w-full h-full text-white text-2xl flex justify-center flex-col'
+            onMouseEnter={ (e) => {setHoverState(true);console.log(classState)}}
+            onMouseLeave={ (e) => {setHoverState(false);console.log(classState)}}
+        >
+            <div>
+            {href.map((value:any, index) => (
+                <div className={index==(pageIndex-1)?'text-indigo-500':'text-white'}>
+                    <a href={value[0]}>
+                        {/*<GoPrimitiveDot className={dotInvisible}/>*/}
+                        {/*<div className={classState}>*/}
+                        {/*    {value[2]}<br/>*/}
+                        {/*    {value[1]}*/}
+                        {/*</div>*/}
 
-            {href.map((value, index) => (
-                <li><div className='dot bg-white'></div></li>
+                        { hoverState ?
+                            <div className='flex flex-col justify-center items-center text-center my-4'>
+                                <div>
+                                    {value[2]}
+                                </div>
+                                <div>
+                                    <small>{value[1]}</small>
+                                </div>
+                            </div> :
+
+                            <GoPrimitiveDot className='my-4 mr-5'/>
+                        }
+                    </a>
+                </div>
             ))}
-            <div className="dot bg-white">sdf</div>
+            </div>
         </div>
     )
 }
