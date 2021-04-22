f8e19ef8b02cfe4f8a3cd47d63af611ea0ff3e01
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-22 17:25:13 +0530

    Visibility check

--- src/index.js
+++ src/index.js
@@ -13,7 +13,22 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-document.body.requestFullscreen();
+window.addEventListener('load', toggleFullScreen());
+
+function toggleFullScreen() {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+    }
+    else {
+        cancelFullScreen.call(doc);
+    }
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
