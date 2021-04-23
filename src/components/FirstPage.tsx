dc91d0fa23c46829c3fd0dc7888c7482e7e92bd5
Prasad Maruti Zore <prasadzore31@gmail.com>
2021-04-23 16:39:00 +0530

    Social media icons added

--- src/components/FirstPage.tsx
+++ src/components/FirstPage.tsx
@@ -1,6 +1,8 @@
 import './FirstPage.css';
 import MyPhoto from '../assets/images/prasad.jpg';
 import VisibilitySensor from 'react-visibility-sensor';
+import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md'
 
 export const FirstPage = (props:any) => {
     const { setPage } = props;
@@ -12,7 +14,7 @@ export const FirstPage = (props:any) => {
     return (
 
         <div className="FirstPage flex justify-center items-center flex-col text-center my-2 viewport-block" id='home'>
-            <div className="mainFrame flex justify-center items-center flex-col px-5 py-10">
+            <div className="mainFrame flex justify-center items-center flex-col px-5 py-10 mt-16">
                 <div className="flex justify-center">
                     <img src={MyPhoto} alt=""
                          className="rounded-full mx-auto absolute -top-28 w-48 h-48 shadow-2xl border-2 border-black"/>
@@ -25,6 +27,20 @@ export const FirstPage = (props:any) => {
                     I am a Tech Enthusiast and a Lifelong tech learner
                 </div>
                 </VisibilitySensor>
+            </div>
+            <div className="flex justify-evenly items-center w-2/5 h-1/5 text-3xl">
+                <div className='twitterIcon bg-black px-3 py-3 rounded-full'>
+                    <a href="https://twitter.com/prasad_zore"><FaTwitter className='text-blue-600'/></a>
+                </div>
+                <div className='githubIcon bg-black px-3 py-3 rounded-full'>
+                    <a href="https://github.com/prasadthx"><FaGithub/></a>
+                </div>
+                <div className='linkedinIcon bg-blue-900 px-3 py-3 rounded-full'>
+                    <a href="https://www.linkedin.com/in/prasad-zore-7b8520191/"><FaLinkedin className='bg-blue-900'/></a>
+                </div>
+                <div className='mailIcon bg-red-800 px-3 py-3 rounded-full'>
+                    <a href="mailto:prasadzore31@gmail.com"><MdEmail/></a>
+                </div>
             </div>
         </div>
 
